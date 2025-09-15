2b74751e8814bc5644a4a7df76964543ca2dff01
miya <tukiya8@yahoo.co.jp>
2025-09-15 17:31:29 +0900

    モデル編集

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -41,4 +41,19 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
+
+    public function purchases()
+    {
+        return $this->hasMany(Purchase::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
