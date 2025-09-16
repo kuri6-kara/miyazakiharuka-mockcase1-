533e713ad72c74415987af8493e7d9cc09769b3c
miya <tukiya8@yahoo.co.jp>
2025-09-16 23:04:56 +0900

    UserController編集

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProfileRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function update(Request $request)
+    public function update(ProfileRequest $request)
     {
         $user = Auth::user();
+        $inputs = $request->validated();
+
         $user->update([
+            'profile_image_path' => $inputs['profile_image_path'] ?? $user->profile_image_path,
+            'name' => $inputs['name'],
+            'postcode' => $inputs['postcode'],
+            'address' => $inputs['address'],
+            'building' => $inputs['building'] ?? null,
             'profile_updated' => true,
         ]);
 
-        return redirect()->route('home')->with('success', 'プロフィールが更新されました。');
+        return redirect()->route('home');
     }
 }
