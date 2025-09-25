3109283aedfec88af4fdc555e7dbe04074884a16
miya <tukiya8@yahoo.co.jp>
2025-09-25 09:46:43 +0900

    商品sold表示編集

--- src/resources/views/items/index.blade.php
+++ src/resources/views/items/index.blade.php
@@ -13,22 +13,12 @@
 
     <div class="items-list">
         @foreach ($items as $item)
-        <div class="item-card sold-out">
+        <div class="item-card @if($item->is_sold) sold-out @endif">
             <a href="{{ route('item.show', ['item_id' => $item->id]) }}">
                 <img src="{{ Storage::url($item->item_image_path) }}" alt="{{ $item->name }}">
                 <p>{{ $item->name }}</p>
-                <p>¥{{ number_format($item->price) }}</p>
-            </a>
-        </div>
-        @endforeach
-
-        @foreach ($items as $item)
-        <div class="item-card @if($item->is_sold) sold-out @endif">
-            <a href="#">
-                <img src="..." alt="商品画像">
-                <p>{{ $item->name }}</p>
                 @if ($item->is_sold)
-                <span class="sold-label">SOLD</span>
+                <p class="sold-label">SOLD</p>
                 @else
                 <p>¥{{ number_format($item->price) }}</p>
                 @endif
