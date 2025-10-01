121d8b4f1beb2dd7f53447f38872d40dfa4716a6
miya <tukiya8@yahoo.co.jp>
2025-10-01 09:12:46 +0900

    ログイン　エラーメッセージ表示修正

--- src/app/Http/Requests/LoginRequest.php
+++ src/app/Http/Requests/LoginRequest.php
@@ -24,7 +24,7 @@ class LoginRequest extends FortifyLoginRequest
     public function rules()
     {
         return [
-            'email' => ['required'],
+            'email' => ['required','email'],
             'password' => ['required'],
         ];
     }
@@ -33,8 +33,8 @@ class LoginRequest extends FortifyLoginRequest
     {
         return [
             'email.required' => 'メールアドレスを入力してください',
-            // 'email.email' => 'メールアドレスの形式が正しくありません',
             'password.required' => 'パスワードを入力してください',
+            'email.email' => 'ログイン情報が登録されていません',
         ];
     }
 }
--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -12,12 +12,6 @@
     <form class="form" action="/login" method="post" novalidate>
         @csrf
 
-        <div class="form__error">
-            @error('email')
-            {{ $message }}
-            @enderror
-        </div>
-
         <div class="form__group">
             <div class="form__group-title">
                 <span class="form__label--item">メールアドレス</span>
@@ -27,8 +21,9 @@
                     <input type="email" name="email" value="{{ old('email') }}" />
                 </div>
                 <div class="form__error">
+                    {{-- 1. メールアドレスの必須エラーのみを表示 (メッセージが「メールアドレスを入力してください」の場合) --}}
                     @error('email')
-                    @if ($message !== 'ログイン情報が登録されていません')
+                    @if ($message === 'メールアドレスを入力してください')
                     {{ $message }}
                     @endif
                     @enderror
@@ -45,12 +40,22 @@
                     <input type="password" name="password" />
                 </div>
                 <div class="form__error">
+                    {{-- 2. パスワードの必須エラーを表示 --}}
                     @error('password')
                     {{ $message }}
+                    @enderror
+
+                    {{-- 3. 認証失敗エラー（@error('email')に紐づくメッセージ）をここで表示 --}}
+                    @error('email')
+                    {{-- メッセージが「メールアドレスを入力してください」ではない場合 == 認証失敗エラーの場合 --}}
+                    @if ($message !== 'メールアドレスを入力してください')
+                    {{ $message }}
+                    @endif
                     @enderror
                 </div>
             </div>
         </div>
+
         <div class="form__button">
             <button class="form__button-submit" type="submit">ログイン</button>
         </div>
