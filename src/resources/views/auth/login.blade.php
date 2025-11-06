399bceded61f0036123fe29a0854fa82cf02f872
miya <tukiya8@yahoo.co.jp>
2025-11-06 17:50:14 +0900

    authフォルダ内ビューファイルの不要コメントアウト削除

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -40,8 +40,6 @@
                     @error('password')
                     {{ $message }}
                     @enderror
-
-                    
                 </div>
             </div>
         </div>
