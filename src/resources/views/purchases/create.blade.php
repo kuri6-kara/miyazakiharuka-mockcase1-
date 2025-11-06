4d363f599b1d39a1caabcaf87ee3e30332af0e36
miya <tukiya8@yahoo.co.jp>
2025-11-06 17:35:33 +0900

    purchasesフォルダ内ビューファイルの不要コメントアウト削除

--- src/resources/views/purchases/create.blade.php
+++ src/resources/views/purchases/create.blade.php
@@ -30,10 +30,8 @@
                     <h2 class="section-heading">支払い方法</h2>
                     <div class="mt-2">
                         <select name="payment_method_id" id="payment_method_select" class="border p-2 rounded-md">
-                            {{-- 選択肢なしの場合にselectedを付与 --}}
                             <option value="" disabled {{ !old('payment_method_id', $selected_payment_method_id) ? 'selected' : '' }}>選択してください</option>
                             @foreach ($payment_methods as $method)
-                            {{-- $selected_payment_method_id を利用して選択状態を保持 --}}
                             <option value="{{ $method->id }}"
                                 {{ (int)old('payment_method_id', $selected_payment_method_id) === $method->id ? 'selected' : '' }}>
                                 {{ $method->payment_method }}
@@ -55,7 +53,6 @@
                     @endif
 
                     <div class="shipping-actions">
-                        {{-- JSでクエリパラメータを付与するため、data-base-url属性を設定 --}}
                         <a href="#" id="change-address-link"
                             data-base-url="{{ route('purchase.edit', ['item_id' => $item->id]) }}"
                             class="change-link">
@@ -105,6 +102,5 @@
 @endsection
 
 @section('script')
-{{-- 統合されたJavaScriptファイルを読み込みます --}}
 <script src="{{ asset('js/purchase_create.js') }}"></script>
 @endsection
--- src/resources/views/purchases/edit.blade.php
+++ src/resources/views/purchases/edit.blade.php
@@ -8,19 +8,14 @@
 <div class="purchase-container">
     <h1 class="purchase-title">送付先住所変更画面</h1>
 
-    {{-- 中央寄せのコンテナ (画像のデザインに合わせる) --}}
     <div class="address-change-card">
         <h2 class="section-heading mb-6 text-center">住所の変更</h2>
 
-        {{-- 修正点1: フォームアクションを既存の POST ルート purchase.update に戻す --}}
         <form method="POST" action="{{ route('purchase.update', ['item_id' => $item->id]) }}">
             @csrf
 
-            {{-- 修正点2: 選択中の payment_method_id を保持するための隠しフィールドを追加 --}}
-            {{-- $selected_payment_method_id は PurchaseController の edit メソッドから渡されます --}}
             <input type="hidden" name="payment_method_id" value="{{ $selected_payment_method_id ?? '' }}">
 
-            {{-- 郵便番号 --}}
             <div class="form-group">
                 <label for="postcode" class="form-label">郵便番号</label>
                 <input type="text" name="postcode" id="postcode"
@@ -31,7 +26,6 @@
                 @enderror
             </div>
 
-            {{-- 住所 --}}
             <div class="form-group">
                 <label for="address" class="form-label">住所</label>
                 <input type="text" name="address" id="address"
@@ -42,7 +36,6 @@
                 @enderror
             </div>
 
-            {{-- 建物名 --}}
             <div class="form-group mb-6">
                 <label for="building" class="form-label">建物名</label>
                 <input type="text" name="building" id="building"
@@ -53,7 +46,6 @@
                 @enderror
             </div>
 
-            {{-- 更新ボタン --}}
             <button type="submit" class="submit-button">更新する</button>
         </form>
     </div>
