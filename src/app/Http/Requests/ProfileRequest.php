da89e7eecfb625a95f1007fa094af85d20404d35
miya <tukiya8@yahoo.co.jp>
2025-11-25 20:29:03 +0900

    プロフィール編集画面エラーメッセージ、一部ファイルのCSS変更

--- src/app/Http/Requests/ProfileRequest.php
+++ src/app/Http/Requests/ProfileRequest.php
@@ -32,4 +32,33 @@ class ProfileRequest extends FormRequest
             'building' => 'nullable|string|max:255',
         ];
     }
+
+    /**
+     * Get the validation messages.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'profile_image.image' => 'プロフィール画像は画像ファイル形式でアップロードしてください。',
+            'profile_image.mimes' => 'プロフィール画像としてアップロードできるファイル形式は、JPEG, PNG のいずれかです。',
+            'profile_image.max' => 'プロフィール画像としてアップロードできるファイルのサイズは2MBまでです。',
+
+            'name.required' => 'ユーザー名は必ず入力してください。',
+            'name.string' => 'ユーザー名は文字列で入力してください。',
+            'name.max' => 'ユーザー名は255文字以内で入力してください。',
+
+            'postcode.required' => '郵便番号は必ず入力してください。',
+            'postcode.string' => '郵便番号は文字列で入力してください。',
+            'postcode.max' => '郵便番号は8文字以内で入力してください。',
+
+            'address.required' => '住所は必ず入力してください。',
+            'address.string' => '住所は文字列で入力してください。',
+            'address.max' => '住所は255文字以内で入力してください。',
+
+            'building.string' => '建物名は文字列で入力してください。',
+            'building.max' => '建物名は255文字以内で入力してください。',
+        ];
+    }
 }
--- src/resources/views/users/edit.blade.php
+++ src/resources/views/users/edit.blade.php
@@ -24,26 +24,41 @@
                     画像を選択する
                 </label>
             </div>
+            @error('profile_image')
+            <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="name">ユーザー名</label>
             <input type="text" name="name" value="{{ old('name', $user->name) }}">
+            @error('name')
+            <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="postcode">郵便番号</label>
             <input type="text" name="postcode" value="{{ old('postcode', $user->postcode) }}">
+            @error('postcode')
+            <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="address">住所</label>
             <input type="text" name="address" value="{{ old('address', $user->address) }}">
+            @error('address')
+            <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="building">建物名</label>
             <input type="text" name="building" value="{{ old('building', $user->building) }}">
+            @error('building')
+            <p class="error-message">{{ $message }}</p>
+            @enderror
         </div>
 
         <button type="submit" class="update-button">更新する</button>
